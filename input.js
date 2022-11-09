60a5f9e54322ef57e460dd21bcb8111697568cfd
AnxhelaAsimi <87004166+AnxhelaAsimi@users.noreply.github.com>
2022-11-09 17:26:54 +0100

    Added Flashing Animation to Arrows

--- input.js
+++ input.js
@@ -5,18 +5,38 @@ window.addEventListener('keydown', e=>{
     switch(e.key){
         case 'ArrowUp':
             if(lastInputDirection.y !== 0) break
+            document.getElementById('arrowUp').classList.add('flash');
+            setTimeout(function() {
+                document.getElementById('arrowUp').classList.remove('flash');
+            }, 500);
+
             inputDirection = {x: 0, y: -1}
             break;
         case 'ArrowDown':
             if(lastInputDirection.y !== 0) break
+            document.getElementById('arrowDown').classList.add('flash');
+            setTimeout(function() {
+                document.getElementById('arrowDown').classList.remove('flash');
+            }, 500);
+
             inputDirection = {x: 0, y: 1}
             break;
         case 'ArrowLeft':
             if(lastInputDirection.x !== 0) break
+            document.getElementById('arrowLeft').classList.add('flash');
+            setTimeout(function() {
+                document.getElementById('arrowLeft').classList.remove('flash');
+            }, 500);
+
             inputDirection = {x: -1, y: 0}
             break;
         case 'ArrowRight':
             if(lastInputDirection.x !== 0) break
+            document.getElementById('arrowRight').classList.add('flash');
+            setTimeout(function() {
+                document.getElementById('arrowRight').classList.remove('flash');
+            }, 500);
+
             inputDirection = {x: 1, y: 0}
             break;        
     }
